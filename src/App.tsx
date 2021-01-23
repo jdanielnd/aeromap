ae2ade791bd491592e91615cec97850de4e4f66e
João Duarte <jdanielnd@gmail.com>
2021-01-22 23:25:36 -0300

    adding hot reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import { hot } from 'react-hot-loader';
 import * as React from 'react';
 
-const App = () => <div>Hi from react !</div>;
+const App = () => <div>Hi from hot react !</div>;
 
-export default App;
+export default hot(module)(App);
